35c2a4b967f7765fef9a3f8451eed0dd5fc0b5f6
supermanshuvo <shuvothesuperman@gmail.com>
2021-03-31 07:14:01 +0600

    Add route in api for get home

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/',[App\Http\Controllers\ItemController::class,'index'])->name('home');
