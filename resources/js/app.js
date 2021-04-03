ac7988a5a374cfd807bb20bc55ad4f5425e14604
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-03 22:16:57 +0600

    Add vue compunecnt and other staf to use vue works

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
+import Vue from 'vue'
+import App from './vue/app'
 require('./bootstrap');
+
+Vue.component('app', require('./vue/app.vue').default);
+const app = new Vue({
+    el: "#app",
+    comments: {App}
+});
