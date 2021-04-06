1a07592da6ae00f2cba6b39b9b2b2c12a320126f
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-06 15:26:44 +0600

    Add function for show item in listime view

--- resources/js/vue/listItem.vue
+++ resources/js/vue/listItem.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
-        List item
+    <div class="item">
+        <input type="checkbox" @change="updateCheck()" v-model="item.completed">
+        <span :class="[item.completed ? 'completed':'','itemText']"
+        >{{ item.name}}</span>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props:['item']
 }
 </script>
