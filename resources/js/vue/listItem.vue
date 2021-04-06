99de35c85159e411c8d811abef7c294cebeff55d
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-06 15:31:09 +0600

    Add some style in the list view

--- resources/js/vue/listItem.vue
+++ resources/js/vue/listItem.vue
@@ -11,3 +11,20 @@ export default {
     props:['item']
 }
 </script>
+
+
+<style scoped>
+.completed{
+    text-decoration: line-through;
+    color: #999999;
+}
+.itemText{
+    width: 100%;
+    margin-left: 20px;
+}
+.item{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
