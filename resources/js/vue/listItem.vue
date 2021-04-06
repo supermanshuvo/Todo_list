19e79a3b1f9a0f0e6476f897b5109a4f3c8bab44
supermanshuvo <shuvothesuperman@gmail.com>
2021-04-06 15:34:20 +0600

    Add trashcan button for delete item

--- resources/js/vue/listItem.vue
+++ resources/js/vue/listItem.vue
@@ -3,6 +3,9 @@
         <input type="checkbox" @change="updateCheck()" v-model="item.completed">
         <span :class="[item.completed ? 'completed':'','itemText']"
         >{{ item.name}}</span>
+        <button @click="removeItem()" class="trashcan">
+            <font-awesome-icon icon="trash"/>
+        </button>
     </div>
 </template>
 
